9dc21e332cd4d5fd07bcae271f1d1381e7c335b9
Hasnain Asif <hasnain.asif@invozone.com>
2022-01-27 17:14:29 +0500

    work on progress

--- algo.js
+++ algo.js
@@ -2,21 +2,21 @@
 
 //// EXAMPLE #1
 //-- Count same fruits
-const fruits = ['apple', 'orange', 'apple', 'mango'];
-const countFruits = fruits.reduce((acc, fruit) => {
-  if (acc[fruit]) {
-    acc[fruit] = acc[fruit] + 1;
-  } else {
-    acc[fruit] = 1;
-  }
+// const fruits = ['apple', 'orange', 'apple', 'mango'];
+// const countFruits = fruits.reduce((acc, fruit) => {
+//   if (acc[fruit]) {
+//     acc[fruit] = acc[fruit] + 1;
+//   } else {
+//     acc[fruit] = 1;
+//   }
 
-  return acc;
-}, {});
+//   return acc;
+// }, {});
 
-console.log('countFruits', countFruits);
+// console.log('countFruits', countFruits);
 
 //-- Count same numbers
-const numbers = [1, 5, 7, 5, 10, 7, 7, 3, 10];
+// const numbers = [1, 5, 7, 5, 10, 7, 7, 3, 10];
 // const countNumbers = numbers.reduce((acc, number) => {
 //   if (acc[number]) {
 //     acc[number] = acc[number] + 1;
