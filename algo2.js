b0f17fe9c6692cd2822a21d6c0d28baef3ea4613
Hasnain Asif <hasnain.asif@tkxel.com>
2022-07-01 16:50:40 +0500

    add multipointer algo

--- algo2.js
+++ algo2.js
@@ -142,32 +142,74 @@
 
 //-- Other way with Better Big-O time
 // No nested loop involve
-const validAnagram = (string1, string2) => {
-  if (string1.length !== string2.length) {
-    return false;
-  }
+// const validAnagram = (string1, string2) => {
+//   if (string1.length !== string2.length) {
+//     return false;
+//   }
 
-  const countString1 = {};
-  const countString2 = {};
-  for (const element1 of string1) {
-    countString1[element1] = (countString1[element1] || 0) + 1;
-  }
-  for (const element2 of string2) {
-    countString2[element2] = (countString2[element2] || 0) + 1;
-  }
+//   const countString1 = {};
+//   const countString2 = {};
+//   for (const element1 of string1) {
+//     countString1[element1] = (countString1[element1] || 0) + 1;
+//   }
+//   for (const element2 of string2) {
+//     countString2[element2] = (countString2[element2] || 0) + 1;
+//   }
 
-  for (const key in countString1) {
-    if (!(key in countString2)) {
-      return false;
-    }
+//   for (const key in countString1) {
+//     if (!(key in countString2)) {
+//       return false;
+//     }
 
-    if (countString1[key] !== countString2[key]) {
-      return false;
-    }
-  }
+//     if (countString1[key] !== countString2[key]) {
+//       return false;
+//     }
+//   }
 
-  return true;
-};
+//   return true;
+// };
 
-console.log(validAnagram('anagram', 'nagaram')); // true
-console.log(validAnagram('rat', 'car')); // false
+// console.log(validAnagram('anagram', 'nagaram')); // true
+// console.log(validAnagram('rat', 'car')); // false
+
+////////////////////////////////////// EXAMPLE # (Multiple Pointers Pattern)
+// Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair
+// where the sum is zero. Return an array that includes both values whose sum is zero or return undefined if no pair exist.
+
+// sumZero([-3, -2, -1, 0, 1, 2, 3]) // [-3, 3]
+// sumZero([-2, 0, 1, 3]) // undefined
+// sumZero([1, 2, 3]) // undefined
+// sumZero([-2, -1, 0, 1, 2, 3, 4]) // [-2, 2]
+// sumZero([-2, -1, 1, 3, 4]); // [-1, 1]
+
+// Solution --
+// start a loop from 1st element to 2nd last element(because last element cannot be compared with anyother element)
+// start a nested loop from last element to 2nd element and add its elements one by one with each element of loop1.
+// if sum of any pair is zero, return those elements in array like [-3, 3].
+// and if no pair exists with sum equal zero, return undefined
+// if any value in array is '0' then no need to compare because if zero is in middle, then sum of zero with zero will be zero.
+
+// const sumZero = (arr) => {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] === 0) {
+//       break;
+//     }
+//     for (let j = arr.length - 1; j >= 1; j--) {
+//       if (Math.abs(arr[i]) > Math.abs(arr[j])) {
+//         // To enhance performance
+//         // as array is sorted, if Math.abs(arr[j]) becomes less than Math.abs(arr[i]), then no need to move further
+//         break;
+//       }
+
+//       if (arr[i] + arr[j] === 0) {
+//         return [arr[i], arr[j]];
+//       }
+//     }
+//   }
+
+//   return undefined;
+// };
+
+// console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
+
+////////////
--- test.js
+++ test.js
@@ -1,34 +1,34 @@
-var cards = [
-  'Jack',
-  8,
-  2,
-  6,
-  'King',
-  5,
-  3,
-  'Queen',
-  'Queen',
-  'Jack',
-  'King',
-  'Queen',
-  'Jack',
-  'King',
-];
+// var cards = [
+//   'Jack',
+//   8,
+//   2,
+//   6,
+//   'King',
+//   5,
+//   3,
+//   'Queen',
+//   'Queen',
+//   'Jack',
+//   'King',
+//   'Queen',
+//   'Jack',
+//   'King',
+// ];
 
-const sortedArray = cards.sort();
-const newArray = [...sortedArray];
+// const sortedArray = cards.sort();
+// const newArray = [...sortedArray];
 
-const kingIndexes = [];
-for (let index = 0; index < sortedArray.length; index++) {
-  if (sortedArray[index] === 'King') {
-    kingIndexes.push(index);
-  }
-}
+// const kingIndexes = [];
+// for (let index = 0; index < sortedArray.length; index++) {
+//   if (sortedArray[index] === 'King') {
+//     kingIndexes.push(index);
+//   }
+// }
 
-for (let i = 0; i < kingIndexes.length; i++) {
-  const index = kingIndexes[0];
-  newArray.splice(index, 1);
-  newArray.push('King');
-}
+// for (let i = 0; i < kingIndexes.length; i++) {
+//   const index = kingIndexes[0];
+//   newArray.splice(index, 1);
+//   newArray.push('King');
+// }
 
-console.log(newArray);
+// console.log(newArray);
